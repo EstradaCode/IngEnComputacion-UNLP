a17fe8e3a6d24ff6b59309266799507d8422fd0a
Francisco <franciscoestrada565@gmail.com>
2024-04-24 13:50:34 -0300

    retomando practica2-Cac

---
+++ Arquitectura-de-Computadoras/practicas/Practica2/ej5.asm
@@ -0,0 +1,68 @@
+org 1000h
+vocales db "AaEeIiOoUu"
+FINVOCALES DB ?
+org 1500h
+CADENA DB ?,?,?
+fin db 0
+
+org 3000h
+ES_VOCAL: PUSH BX
+          PUSH CX; CL, DATO CARACTER
+          ; AH RESULT TRUE O FALSE
+          MOV BX, OFFSET VOCALES
+          MOV CH, OFFSET FINVOCALES -OFFSET VOCALES
+          CMP CH, 0
+          JZ FIN_ESVOCAL
+LOOP_ESVOCAL: CMP BYTE PTR [BX], CL
+              JNZ NEXT_VOCAL
+              MOV AH, 0FFH
+              JMP FIN_ESVOCAL
+NEXT_VOCAL:  INC BX
+              DEC CH
+              JNZ LOOP_ESVOCAL
+              MOV AH, 00H
+FIN_ESVOCAL: POP CX
+            POP BX
+            RET; RETORNO POR AH RESULT
+
+
+
+CONTAR_VOC: PUSH BX
+            PUSH CX
+            MOV AX, 0
+            CMP CH, 0
+            JZ FINCONTAR
+LOOP_CONTAR: MOV CL, [BX] 
+            CALL ES_VOCAL
+            CMP AH, 0FFH
+            JNZ NEXT
+            INC AL
+      NEXT: INC BX
+            DEC CH
+            JNZ LOOP_CONTAR
+FINCONTAR:POP CX
+          POP BX
+          RET ; devuelve por AL la cantidad
+
+
+
+leer_str_n: PUSH BX ; BX TIENE DIRECCION DEL 1ER ELEM DE LA CADENA
+            PUSH CX; CH TIENE LA CANTIDAD A LEER
+            CMP CH, 0
+            JZ FIN_STR
+      LOOP: INT 6
+            INC BX ; AVANZO
+            DEC CH
+            JNZ LOOP
+FIN_STR: POP CX
+         POP BX
+         RET
+ORG 2000H
+MOV BX, OFFSET CADENA
+MOV CX, 0
+MOV CH, OFFSET FIN - OFFSET CADENA
+call leer_str_n
+call CONTAR_VOC
+INT 7
+hlt
+end
