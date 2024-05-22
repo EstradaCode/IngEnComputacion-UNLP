9ded7b6cfe70659063ae95263194cddb3d5aa2ab
EstradaCode <franciscoestrada565@gmail.com>
2024-05-22 15:16:42 -0300

    prograIII add p3ej1

---
+++ Programacion-III/practica3/src/tp3/ej1/GenTest.java
@@ -0,0 +1,43 @@
+package tp3.ej1;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class GenTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GeneralTree<Integer> g = new GeneralTree<Integer>(5);
+		g.isEmpty();
+		List <GeneralTree<Integer>> children = new ArrayList < GeneralTree<Integer>>();
+		children.add(new GeneralTree<Integer>(4));
+		children.add(new GeneralTree<Integer>(1));
+		children.add(new GeneralTree<Integer>(6));
+		children.add(new GeneralTree<Integer>(9));
+		g = new GeneralTree<Integer>(5, children);
+		children = new ArrayList < GeneralTree<Integer>>();
+		children.add(new GeneralTree<Integer>(11));
+		children.add(new GeneralTree<Integer>(10));
+		children.add(new GeneralTree<Integer>(15));
+		children.add(new GeneralTree<Integer>(19));
+		List <GeneralTree<Integer>> list  = g.getChildren();
+		int i = 0;
+		for (GeneralTree<Integer> c : list) {
+			c.addChild(children.get(i++));
+		}
+		// recorrer 
+		List <Integer> l = new ArrayList <Integer>();
+		preorder(g,l);
+		System.out.println(l);
+		
+	}
+	public static void preorder (GeneralTree<Integer> root, List<Integer> l){
+		l.add(root.getData());
+		List <GeneralTree<Integer>> list  = root.getChildren();
+		for (GeneralTree<Integer> c : list) {
+			preorder(c, l);
+		}		
+	}
+
+}
---
+++ Programacion-III/practica3/src/tp3/ej1/GeneralTree.java
@@ -0,0 +1,64 @@
+package tp3.ej1;
+
+import java.util.List;
+import java.util.LinkedList;
+
+
+public class GeneralTree<T> {
+
+	private T data;
+	private List<GeneralTree<T>> children = new LinkedList<GeneralTree<T>>();
+
+	public GeneralTree(T data) {
+		this.data = data;
+	}
+
+	public GeneralTree(T data, List<GeneralTree<T>> children) {
+		this(data);
+		this.children = children;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public void setChildren(List<GeneralTree<T>> children) {
+		if (children != null)
+			this.children = children;
+	}
+
+	public List<GeneralTree<T>> getChildren() {
+		return this.children;
+	}
+
+	public void addChild(GeneralTree<T> child) {
+		this.getChildren().add(child); // añade un hijo a la lista de hijos
+	}
+
+	public boolean isLeaf() {
+		return !this.hasChildren(); // si no tiene hijos es hoja
+	}
+
+	public boolean hasChildren() {
+		return children != null && !children.isEmpty();
+	}
+
+	public boolean isEmpty() {
+		return this.data == null && !hasChildren();
+	}
+
+	public void removeChild(GeneralTree<T> child) {
+		if (this.hasChildren()) { // si tiene hijos
+			List<GeneralTree<T>> children = this.getChildren(); // obtengo la lista de hijos
+			if (children.contains(child)) // si lo tiene lo borro
+				children.remove(child);
+		}
+	}
+
+	
+
+}
