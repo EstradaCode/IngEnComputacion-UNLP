c43e9917437d03182dc13e9aa219f4a80613644e
EstradaCode <franciscoestrada565@gmail.com>
2025-01-23 01:16:13 -0300

    CSO- P3, reiniciando

---
+++ Conceptos-de-Sistemas-Operativos/Practicas/Practica-3/scriptsft/ej3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Comentarios acerca de lo que hace el script
+# Siempre comento mis scripts, si no hoy lo hago
+# y mañana ya no me acuerdo de lo que quise hacer
+echo "Introduzca su nombre y apellido:"
+read nombre apellido
+echo "Fecha y hora actual:"
+date
+echo "Su apellido y nombre es:"
+echo "$apellido $nombre"
+echo "Su usuario es: `whoami`"
+echo "Su directorio actual es:"
+pwd
+echo "contenido de la carpeta:"
+ls
+echo "espacio libre del disco:"
+df -h
+echo "espacio utilizado por el directorio del disco:"
+du -sh
+
